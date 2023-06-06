c0a789900eed2015f7efffaa0a4a222a15ddf1f0
Harunericek <77488069+Harunericek@users.noreply.github.com>
2023-06-06 13:38:51 +0200

    Changed Contact Page

--- src/components/Contact/Middle.jsx
+++ src/components/Contact/Middle.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 function Middle() {
     return <div className="middle-container">
+  <div>
+    <form className="flex-container" action="onSubmit">
+      <input className="item1" placeholder="Vorname" type="text" />
+      <input className="item2" placeholder="Nachname" type="text" />
+      <input className="item3" placeholder="Email" type="text" />
+      <button className="navbtn">Submit</button>
+    </form>
+  </div>
     </div>
 }
 
