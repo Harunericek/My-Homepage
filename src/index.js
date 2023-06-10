ddea55c102af6aafdd70d00b919b59bdfc6050d7
Harunericek <77488069+Harunericek@users.noreply.github.com>
2023-06-10 17:56:31 +0200

    test

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
 <header>
     <nav style={{position: "absolute", zIndex: "1", left: "36%", top: "7%"}}>
-    <Link className="nav-btn" to="/">Home</Link>
+    <Link className="nav-bn" to="/">Home</Link>
     <Link className="nav-btn" to="/Projects">Projects</Link>
     <Link className="nav-btn" to="/Contact">Contact</Link>
     </nav>
